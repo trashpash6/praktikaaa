103a26d5ca5ccde4020eb0c79a92ed8cffe0e9d4
trashpash6 <113009307+trashpash6@users.noreply.github.com>
2023-07-06 13:54:54 +0600

    Added test2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Timer from './taimer';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Taimer</h1>
+      <Timer />
+    </div>
+  );
+};
+export default App;
